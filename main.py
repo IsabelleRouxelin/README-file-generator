ab87deee68a254c701cdab2813483ec1be134e19
IsabelleRouxelin <isabelle.rouxelin@gmail.com>
2025-07-10 23:15:59 +0100

    created main python file

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+#get user input - including title, description, installation, usage, license, author, and contact info.
+#A dropdown list is used to select a license type.
+#A `README.md` file is generated using GitHub markdown format based on the user's input.
+# The codebase is structured using classes and organized into modules.
+#A `requirements.txt` file is included listing all dependencies (including `PyInquirer`).
+# A virtual environment is used for the project, and its setup is documented.
+# Instructions for running the application are included.
+#A demo video of the script running is submitted.
+#A link to the GitHub repository is included with the submission.
